8e84e3241fc4e7c39eaf5fbc5892ebc2a1e7ec8e
carl <mcjcruz1@tip.edu.ph>
2020-05-31 01:20:11 +0000

    renewed logic for detailed beakdown

--- ppe_project/stocks/urls.py
+++ ppe_project/stocks/urls.py
@@ -9,6 +9,11 @@ urlpatterns = [
     # equipment urls
     path('add_equipment/', views.add_equipment, name='add_equipment'),
     path('show_all_equipment/', views.show_all_equipment, name='show_all_equipment'),
+    path('use_equipment/', views.use_equipment, name='use_equipment'),
+
+    path('detailed_equipment/<int:id>', views.detailed_equipment, name='detailed_equipment'),
+
+
 
 
 ]
--- ppe_project/stocks/views.py
+++ ppe_project/stocks/views.py
@@ -38,6 +38,40 @@ def add_equipment(request):
     return render(request, template_name, context=context)
 
 
+def detailed_equipment(request, id):
+    template_name = 'equipment/detailed_equipment.html'
+    equipment = Equipment.objects.get(id=id)
+    total_used_equipment = Used.objects.all().filter(equipment=id)
+    total_used = 0
+    for use in total_used_equipment:
+        total_used += use.qty_to_be_used
+
+    total_available = equipment.qty_per_package - total_used
+    
+    # detailed = list(total_used_equipment)
+
+    # detailed_qty = []
+    # detailed_date = []
+    # for detailed_use in detailed:
+    #     date = detailed_use.qty_to_be_used
+    #     quantity = detailed_use.date_being_used
+        
+    #     detailed_date.append(date)
+    #     detailed_qty.append(quantity)
+
+
+
+    context = {
+        'equipment': equipment,
+        'total_used': total_used,
+        'total_available': total_available,
+        'total_used_equipment': total_used_equipment,
+    }
+
+    return render(request, template_name, context=context)
+
+
+
 
 def use_equipment(request):
     template_name = 'equipment/use_equipment.html'
